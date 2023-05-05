9db730e5feb5d057bc8e880fda0d1351102a5acb
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-05 18:14:09 +0200

    section 05: get auctions that should be closed using query on statusAndEndDate Index

---
+++ auction-service/src/lib/getEndedAuctions.js
@@ -0,0 +1,27 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export async function getEndedAuctions() {
+  const now = new Date();
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: "statusAndEndDate",
+    KeyConditionExpression: "#status = :status AND endingAt <= :now",
+    ExpressionAttributeValues: {
+      ":status": "OPEN",
+      ":now": now.toISOString(),
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+  };
+
+  const command = new QueryCommand(params);
+
+  const result = await docClient.send(command);
+  return result.Items;
+}
