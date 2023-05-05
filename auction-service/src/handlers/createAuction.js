c6f8d0d05405d9439e233085ae865ff2b10f93d1
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-05 20:20:07 +0200

    section 05: validate createAuction body

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,6 +1,9 @@
 import { v4 as uuid } from "uuid";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import validator from "@middy/validator";
+import { transpileSchema } from "@middy/validator/transpile";
+import createAuctionSchema from "../lib/schemas/createAuctionSchema";
 import commonMiddleware from "../lib/commonMiddleware";
 import createError from "http-errors";
 
@@ -44,4 +47,8 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction).use(
+  validator({
+    eventSchema: transpileSchema(createAuctionSchema),
+  })
+);
---
+++ auction-service/src/lib/schemas/createAuctionSchema.js
@@ -0,0 +1,18 @@
+const schema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        title: {
+          type: "string",
+          minLength: 1,
+        },
+      },
+      required: ["title"],
+    },
+  },
+  required: ["body"],
+};
+
+export default schema;
