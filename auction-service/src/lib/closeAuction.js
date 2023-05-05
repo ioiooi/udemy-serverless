c184a6fb0cde1c9568193888f38db5b098e7c729
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-05 18:35:58 +0200

    section 05: close auctions

---
+++ auction-service/src/lib/closeAuction.js
@@ -0,0 +1,23 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+export async function closeAuction(auction) {
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id: auction.id },
+    UpdateExpression: "set #status = :status",
+    ExpressionAttributeValues: {
+      ":status": "CLOSED",
+    },
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  const command = new UpdateCommand(params);
+  return docClient.send(command);
+}
