7a5d147663a5138eb649c4f53b6a812fd3fdf973
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-20 19:54:05 +0200

    section 07: Create SQS and update lambda to handle incoming SQS events

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import { SES } from "@aws-sdk/client-ses";
 const ses = new SES({ region: "eu-central-1" });
 
 async function sendMail(event, context) {
+  console.log(event);
+
+  const record = event.Records[0];
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
     Source: "pehajew370@asuflex.com",
     Destination: {
-      ToAddresses: ["pehajew370@asuflex.com"],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: "Hello from Codingly!",
+          Data: body,
         },
       },
       Subject: {
-        Data: "Test Mail",
+        Data: subject,
       },
     },
   };
