d13141b0cc992f155d8f338b2201cdefe155ba8a
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-21 16:30:44 +0200

    section 07: Send SQS messages to notify seller and bidder at the closing of a auction

--- auction-service/src/lib/closeAuction.js
+++ auction-service/src/lib/closeAuction.js
@@ -1,11 +1,13 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
+const sqs = new SQSClient({ region: "eu-central-1" });
 
 export async function closeAuction(auction) {
-  const params = {
+  const closeAuctionsInput = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id: auction.id },
     UpdateExpression: "set #status = :status",
@@ -18,6 +20,48 @@ export async function closeAuction(auction) {
     ReturnValues: "ALL_NEW",
   };
 
-  const command = new UpdateCommand(params);
-  return docClient.send(command);
+  const closeAuctionsCommand = new UpdateCommand(closeAuctionsInput);
+  await docClient.send(closeAuctionsCommand);
+
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  if (amount === 0) {
+    const input = {
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: "No bids on your auction item :(",
+        recipient: seller,
+        body: `Oh no! Your item "${title}" didn't get any bids. Better luck next time!`,
+      }),
+    };
+    const command = new SendMessageCommand(input);
+    await sqs.send(command);
+
+    return;
+  }
+
+  const notifySellerInput = {
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: "Your item has been sold!",
+      recipient: seller,
+      body: `Woohoo! Your itme "${title}" has been sold for $${amount}.`,
+    }),
+  };
+  const notifySellerCommand = new SendMessageCommand(notifySellerInput);
+  const notifySeller = sqs.send(notifySellerCommand);
+
+  const notifyBidderInput = {
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: "You won an auction!",
+      recipient: bidder,
+      body: `What a great deal! You got yourself a "${title}" for $${amount}.`,
+    }),
+  };
+  const notifyBidderCommand = new SendMessageCommand(notifyBidderInput);
+  const notifyBidder = sqs.send(notifyBidderCommand);
+
+  return Promise.all([notifySeller, notifyBidder]);
 }
