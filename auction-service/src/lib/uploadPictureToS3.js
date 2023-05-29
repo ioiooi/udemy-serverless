89518e776f9f0849cdac9fabc55059eedb5388a1
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-29 17:03:35 +0200

    section 08: Upload pictures as base64 and save them in S3

---
+++ auction-service/src/lib/uploadPictureToS3.js
@@ -0,0 +1,19 @@
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const client = new S3Client();
+
+export async function uploadPictureToS3(key, body) {
+  const params = {
+    Bucket: process.env.AUCTIONS_BUCKET_NAME,
+    Key: key,
+    Body: body,
+    ContentEncoding: "base64",
+    ContentType: "image/jpeg",
+  };
+
+  return new Upload({
+    client,
+    params,
+  }).done();
+}
