44eb54eac7a58fb18e71e59eef32f9a5a1bffd8f
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-29 18:45:38 +0200

    section 08: Add braindead validation for base64 body

--- auction-service/src/handlers/uploadAuctionPicture.js
+++ auction-service/src/handlers/uploadAuctionPicture.js
@@ -1,13 +1,23 @@
 import middy from "@middy/core";
 import httpErrorHandler from "@middy/http-error-handler";
 import createError from "http-errors";
+import validator from "@middy/validator";
+import { transpileSchema } from "@middy/validator/transpile";
+import inputOutputLogger from "@middy/input-output-logger";
 import { getAuctionById } from "./getAuction";
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
 import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
+import uploadAuctionPictureSchema from "../lib/schemas/uploadAuctionPictureSchema";
 
 export async function uploadAuctionPicutre(event) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer.lambda;
   const auction = await getAuctionById(id);
+
+  if (email !== auction.seller) {
+    throw new createError.Forbidden("You are not the seller of this auction!");
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
   const buffer = Buffer.from(base64, "base64");
 
@@ -17,7 +27,7 @@ export async function uploadAuctionPicutre(event) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(updatedAuction),
+      body: JSON.stringify(updatedAuction.Attributes),
     };
   } catch (error) {
     console.log(error);
@@ -25,4 +35,10 @@ export async function uploadAuctionPicutre(event) {
   }
 }
 
-export const handler = middy(uploadAuctionPicutre).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicutre).use([
+  httpErrorHandler(),
+  validator({
+    eventSchema: transpileSchema(uploadAuctionPictureSchema),
+  }),
+  inputOutputLogger(),
+]);
