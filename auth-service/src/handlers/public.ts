fb83bf37e9013d0c517ea76955628fc6fd871cc6
Eugen Mayer <eugen.mayer2@gmail.com>
2023-05-31 23:11:08 +0200

    Move to typescript

---
+++ auth-service/src/handlers/public.ts
@@ -0,0 +1,16 @@
+import { APIGatewayProxyResult } from "aws-lambda";
+
+export async function handler(): Promise<APIGatewayProxyResult> {
+  return {
+    statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      "Access-Control-Allow-Origin": "*",
+      /* Required for cookies, authorization headers with HTTPS */
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify({
+      message: "Hi ⊂◉‿◉つ from Public API",
+    }),
+  };
+}
